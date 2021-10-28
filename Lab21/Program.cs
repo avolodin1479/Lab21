671b0d522486dd3d796b8b3160c921379351c2eb
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-28 16:22:25 +0600

    Добавьте файлы проекта.

---
+++ Lab21/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab21
+{
+    class Program
+    {
+        private static int[,] field;
+        static void Main(string[] args)
+        {
+            field = new int[10, 8];
+            Thread gardener1 = new Thread(Gardener1);
+            Thread gardener2 = new Thread(Gardener2);
+            gardener1.Start();
+            gardener2.Start();
+
+            gardener1.Join();
+            gardener2.Join();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write("{0}  ", field[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        private static void Gardener1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (field[i, j] == 0)
+                    {
+                        field[i, j] = 1;
+                    }
+                    Thread.Sleep(1);
+                }
+            }
+        }
+        private static void Gardener2()
+        {
+            for (int i = 7; i > 0; i--)
+            {
+                for (int j = 9; j > 0; j--)
+                {
+                    if (field[j, i] == 0)
+                    {
+                        field[j, i] = 2;
+                    }
+                    Thread.Sleep(1);
+                }
+            }
+        }
+    }
+}
